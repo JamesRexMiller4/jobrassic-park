d1263262c4219df9fb4b716e69d1a5817b286dd0
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2020-06-19 17:35:40 -0600

    Add realm dir and attempt apollo client setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import gql from 'graphql-tag';
 import { useQuery } from '@apollo/react-hooks';
+import { createApolloClient } from './realm/realmApolloClient';
+
+import * as RealmWeb from "realm-web";
+
+const REALM_APP_ID = "jobrassic_park_db";
+const App = new RealmWeb.App({ id: REALM_APP_ID });
+
 
 const GET_QUERY = gql`
   query {
@@ -23,16 +30,20 @@ interface job {
   date_posted: Date
 }
 
-function App() {
+function app() {
 
   const { data } = useQuery<job>(GET_QUERY)
 
   console.log(data)
 
   return (
+    <App>
+    <div id='root'>
         <p>
           {data}
         </p>
+    </div>
+    </App>
   );
 }
 
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,28 +4,28 @@ import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker';
 // 1
-import { ApolloProvider } from 'react-apollo'
-import { ApolloClient } from 'apollo-client'
-import { createHttpLink } from 'apollo-link-http'
-import { InMemoryCache } from 'apollo-cache-inmemory'
+// import { ApolloProvider } from 'react-apollo'
+// import { ApolloClient } from 'apollo-client'
+// import { createHttpLink } from 'apollo-link-http'
+// import { InMemoryCache } from 'apollo-cache-inmemory'
 
-const API_PASSWORD = process.env.REACT_APP_API_PASSWORD
+// const API_PASSWORD = process.env.REACT_APP_API_PASSWORD
 
-// 2
-const httpLink = createHttpLink({
-  uri: `mongodb+srv://giannuz1:${API_PASSWORD}@cluster0-omm8r.mongodb.net/jobrassic_park?retryWrites=true&w=majority`
-})
-// 3
-const client = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache()
-})
-// 4
+// // 2
+// const httpLink = createHttpLink({
+//   uri: `http://mongodb+srv://giannuz1:${API_PASSWORD}@cluster0-omm8r.mongodb.net/jobrassic_park_db?retryWrites=true&w=majority`
+// })
+// // 3
+// const client = new ApolloClient({
+//   link: httpLink,
+//   cache: new InMemoryCache()
+// })
+// // 4
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
+  <React.StrictMode>
     <App />
-  </ApolloProvider>,
+  </React.StrictMode>,
   document.getElementById('root')
 )
 serviceWorker.unregister();
