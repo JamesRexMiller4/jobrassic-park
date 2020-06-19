7ef39d588d7e8c757f689beb06266d97ed126f6f
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2020-06-19 16:18:51 -0600

    Add env file and var with password

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/react-hooks';
+
+const GET_QUERY = gql`
+  query {
+    job {
+      _id
+      date_posted
+      description
+      job_title
+      name
+      url
+    }
+  }
+`
+
+interface job {
+  id: string,
+  description: string,
+  url: string,
+  name: string,
+  date_posted: Date
+}
 
 function App() {
+
+  const { data } = useQuery<job>(GET_QUERY)
+
+  console.log(data)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {data}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
   );
 }
 
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
 import * as serviceWorker from './serviceWorker';
+// 1
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+
+const API_PASSWORD = process.env.REACT_APP_API_PASSWORD
+
+// 2
+const httpLink = createHttpLink({
+  uri: `mongodb+srv://giannuz1:${API_PASSWORD}@cluster0-omm8r.mongodb.net/jobrassic_park?retryWrites=true&w=majority`
+})
+// 3
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+})
+// 4
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+)
 serviceWorker.unregister();
+
+
